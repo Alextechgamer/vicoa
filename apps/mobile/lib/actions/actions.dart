b8b87ecef2ccc6363c7a062126e6d5813add3716
Nick <nicktay.tech@gmail.com>
2026-09-20 17:46:26 +0800

    chore(mobile): bump to 1.7.10 (build 35) and add v1.7.10 changelog

--- apps/mobile/lib/actions/actions.dart
+++ apps/mobile/lib/actions/actions.dart
@@ -72,7 +72,7 @@ Future postAuth(
 Future checkVersion(BuildContext context) async {
   String? latestVersion;
 
-  FFAppState().appVersion = '1.7.9';
+  FFAppState().appVersion = '1.7.10';
   if (FFAppState().appVersion == FFAppState().setting.versionNotified) {
     return;
   }
